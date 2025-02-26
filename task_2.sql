c307cb03ef5355fa47f87501057336ace50ad13b
Rachael <152806664+TheRachell@users.noreply.github.com>
2025-02-26 21:48:42 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -15,9 +15,9 @@ CREATE TABLE IF NOT EXISTS Books (
 );
 
 CREATE TABLE IF NOT EXISTS Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR (215) NOT NULL,
     email VARCAHR (215) UNIQUE NOT NULL,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     address TEXT 
 );
 
