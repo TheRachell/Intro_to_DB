616901c3a98f175a2b6c9ceafd0e511b3b1ba7b5
Rachael <152806664+TheRachell@users.noreply.github.com>
2025-02-28 15:15:29 +0100

    adding data to the customers table

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+INSERT INTO Customers (customer_name, email, address) 
+VALUES 
+    ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
